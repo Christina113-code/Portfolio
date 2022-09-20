e6778c7301cea753fa9df2c2a7219819b9a4fbb5
Christina <christinajun113@gmail.com>
2022-09-20 17:26:08 -0400

    Hero section layout

---
+++ src/Sections/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../Components/Button'
+import heroimg2 from '../imgs/heroimg2.svg'
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+    <img id="hero-img1"alt="girl coding"src={heroimg2}/>
+    <h1>Hello I'm Christina</h1>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    </p>
+    <Button text="See Projects"/>
+    </div>
+  )
+}
+
+export default HeroSection
