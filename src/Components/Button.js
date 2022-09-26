f3a387b70f8e2db9175c3b4530c9460c3edf1e8d
Christina <christinajun113@gmail.com>
2022-09-26 09:56:31 -0400

    put in first proj, resume link works

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,8 +3,8 @@ import arrow from '../imgs/rightarrow.svg'
 const Button = (props) => {
     return(
 <>
-{props.primary? <button className="primary btn"><a href='#projects' style={{color:'white'}}>{props.text}</a></button>
-    :props.secondary? <button className="secondary btn">{props.text}</button>:
+{props.primary?<a className="button-anchor" href='#projects' style={{color:'white'}}> <button className="primary btn">{props.text}</button></a>
+    :props.secondary? <a className="resume-btn"href='https://1drv.ms/b/s!Av1f2KJivTOJnOM0Vwy65_P3np_qYQ?e=iS7Ha8'  target={'_blank'} rel="noreferrer"style={{color:'var(--clr-prmy)'}}><button className="secondary btn">{props.text}</button></a>:
     <button className="normal btn">{props.text}<img id="right-arrow"alt="arrow"src={arrow}/></button>
     }
         </>
--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import img from '../imgs/heroimg2.svg';
+import calc from '../imgs/tip-calc-screenshot.png';
 const Projects = () => {
   return (
     <div className="projects section" id='projects'>
       <h2>My Projects</h2>
       <div className="project-container">
           <div className="project">
-            <a href="/"><img alt="blah"src={img}/></a>
-            <a href="/">Project 1</a>
+            <a className="proj-img"href="https://christina113-code.github.io/tip-calculator/" target={"_blank"}rel="noreferrer"><img alt="blah"src={calc}/></a>
+            <a className="proj-img" href="https://christina113-code.github.io/tip-calculator/" target={"_blank"}rel="noreferrer">Tip Calculator</a>
           </div>
           <div className="project">
-            <a href="/"><img alt="blah"src={img}/></a>
-            <a href="/">Project 1</a>
+            <a className="proj-img" href="/"><img alt="blah"src={img}/></a>
+            <a className="proj-img" href="/">Project 1</a>
           </div>
           <div className="project">
-            <a href="/"><img alt="blah"src={img}/></a>
-            <a href="/">Project 1</a>
+            <a className="proj-img" href="/"><img alt="blah"src={img}/></a>
+            <a className="proj-img" href="/">Project 1</a>
           </div>
           <div className="project">
-            <a href="/"><img alt="blah"src={img}/></a>
-            <a href="/">Project 1</a>
+            <a className="proj-img" href="/"><img alt="blah"src={img}/></a>
+            <a className="proj-img" href="/">Project 1</a>
           </div>
       </div>
     </div>
